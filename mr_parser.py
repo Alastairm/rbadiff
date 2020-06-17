f6a3b5f4637b8fe444aeaf98f55583e95e259455
Alastair Mory <alastair.mory@gmail.com>
2020-06-17 17:34:05 +0800

    bugfixes

--- mr_parser.py
+++ mr_parser.py
@@ -1,5 +1,6 @@
 from datetime import date
 from html.parser import HTMLParser
+import re
 from urllib import request
 
 
@@ -9,48 +10,49 @@ class RBAMRParser(HTMLParser):
     statement = Statement()
     in_content = False
 
-    def handle_starttag(self, tag, attrs):
-        attrs = dict(attrs)
-
-        if tag is 'div' and 'itemprop' in attrs.keys() and attrs['itemprop'] is 'text':
-            self.in_content = True
-
     def handle_startendtag(self, tag, attrs):
         attrs = dict(attrs)
         
-        if tag is 'meta' and 'name' in attrs.keys() and 'content' in attrs.keys():
+        if tag == 'meta' and 'name' in attrs.keys() and 'content' in attrs.keys():
             name = attrs['name']
             content = attrs['content']
-            if name is 'dcterms.identifier':
+            if name == 'dcterms.identifier':
                 self.statement.url = content
-            if name is 'dcterms.title':
+            if name == 'dcterms.title':
                 self.statement.title = content
-            if name is 'dcterms.description':
+            if name == 'dcterms.description':
                 self.statement.description = content
-            if name is 'dcterms.created':
-                pub_date = date(*content.split('-'))
+            if name == 'dcterms.created':
+                pub_date = date(*list(map(int, content.split('-'))))
                 self.statement.pub_date = pub_date
 
+    def handle_starttag(self, tag, attrs):
+        attrs = dict(attrs)
 
+        if tag == 'div' and 'itemprop' in attrs.keys() and attrs['itemprop'] == 'text':
+            self.in_content = True
 
     def handle_endtag(self, tag):
         if self.in_content:
-            if tag is 'div':
+            if tag == 'div':
                 self.in_content = False
-            elif tag is 'p':
+            elif tag == 'p':
                 self.statement.content += '\n'
 
     def handle_data(self, data):
         if self.in_content:
+            data = re.sub(r'\s+', ' ', data)
             self.statement.content += data
 
+
 def parse_url(url):
+    print(f"GET: {url}")
     page = request.urlopen(url)
 
     page_str = ''
     for line in page.readlines():
         page_str += line.decode('utf-8')
-    
+    print(f"PAGE:{page_str}")
     parser = RBAMRParser()
     parser.feed(page_str)
 
